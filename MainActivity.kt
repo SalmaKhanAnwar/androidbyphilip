26167fcd929e69ea635c7e600b9541d588e172ea
Salma Khan <116517414+SalmaKhanAnwar@users.noreply.github.com>
2023-05-17 05:04:11 +0530

    list

---
+++ MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.list.ui.theme.ListTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var scrollstate= rememberScrollState()
+            Column(modifier = Modifier.verticalScroll(scrollstate) ) {
+                for (i in 1..50){
+                    Text(text = "item $i"  ,
+                    fontSize = 24.sp ,
+                    fontWeight = FontWeight.Bold ,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(vertical = 20.dp)
+                        .fillMaxWidth())
+                }
+            }
+        }}}
